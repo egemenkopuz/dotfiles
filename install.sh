a96c7b92ea7a1d58d95c90503de13f65b3dbd0fe
Egemen Kopuz <egemen.kopuz@tum.de>
2022-12-25 17:24:40 +0300

    hotfix: symlink for nvim in install script

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ln -sf $HOME/dotfiles/antigen/.antigenrc $HOME/.antigenrc
 ln -sf $HOME/dotfiles/p10k/.p10k.zsh $HOME/.p10k.zsh
 ln -sf $HOME/dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -sf $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
-ln -s $HOME/dotfiles/nvim $HOME/.config
+ln -s $HOME/dotfiles/nvim $HOME/.config/
