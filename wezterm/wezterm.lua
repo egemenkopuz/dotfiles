e32f67ab417d62fc138a53d43098f68024357cab
Egemen Kopuz <egemenkopuz@hotmail.com>
2023-04-08 21:47:09 +0200

    refactor(wezterm): change terminal background color to align with neovim theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,21 +1,21 @@
 local wezterm = require("wezterm")
 
 local color_scheme = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-color_scheme.background = "#242933"
+color_scheme.background = "#181616"
 
 return {
-    default_domain = "WSL:Ubuntu-22.04",
-    font = wezterm.font("JetBrainsMono Nerd Font"),
-    color_schemes = { custom = color_scheme },
-    color_scheme = "custom",
-    font_size = 10,
-    line_height = 1.2,
-    window_padding = { left = 1, right = 1, top = 0, bottom = 0 },
-    check_for_updates = false,
-    enable_tab_bar = false,
-    animation_fps = 1,
-    cursor_blink_rate = 0,
-    cursor_blink_ease_in = "Constant",
-    cursor_blink_ease_out = "Constant",
-    disable_default_key_bindings = false,
+	default_domain = "WSL:Ubuntu-22.04",
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	color_schemes = { custom = color_scheme },
+	color_scheme = "custom",
+	font_size = 10,
+	line_height = 1.2,
+	window_padding = { left = 1, right = 1, top = 0, bottom = 0 },
+	check_for_updates = false,
+	enable_tab_bar = false,
+	animation_fps = 1,
+	cursor_blink_rate = 0,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	disable_default_key_bindings = false,
 }
